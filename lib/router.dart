f720c924a6e0f9a166eb83bd671d1c4f3d7115d6
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2020-08-02 15:24:22 +0100

    improvements

--- lib/router.dart
+++ lib/router.dart
@@ -2,10 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_package/model/domain/package.dart';
 import 'package:flutter_package/view/packages/all_packages.dart';
+import 'package:flutter_package/view/packages/details_package.dart';
 import 'package:flutter_package/view/settings_screen.dart';
 import 'package:flutter_package/view/splash_view_screen.dart';
-import 'main.dart';
-import 'view/packages/details_package.dart';
 
 class Router {
   static const RouterPackageViewScreen = 'PackageViewScreen';
