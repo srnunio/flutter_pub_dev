2bd05040d7d4b2bcd29e5a54c8f11863f3f6fab4
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2021-12-31 09:18:34 +0100

    insert comment search domain

--- lib/src/domain/search/i_search_service.dart
+++ lib/src/domain/search/i_search_service.dart
@@ -6,8 +6,11 @@ import 'package:flutter_package/src/domain/core/request_failure.dart';
 abstract class ISearchService extends ApiService {
   ISearchService(Dio dio) : super(dio);
 
-  final String path = 'search';
+  final String path = '/search';
 
+  /// [searchPackage] Method responsible for searching packages
+  /// This returns list of package names when the request is successful
+  /// and [RequestFailure] when the request is not successful
   Future<Either<RequestFailure, List<String>>> searchPackage(
       {required int page, required String query});
 }
