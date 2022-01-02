032fdad812cb4aedf09a1b09ad20d2d47872ff74
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2022-01-02 17:56:07 +0100

    fix custom progress

--- lib/src/presentation/core/custom_progress.dart
+++ lib/src/presentation/core/custom_progress.dart
@@ -2,43 +2,35 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_package/src/utils/colors.dart';
 
 class CustomProgress extends StatelessWidget {
   final Color? color;
 
   final double size;
 
-  final bool ignorePlatform;
-
-  const CustomProgress(
-      {this.color, this.size = 20, this.ignorePlatform = false});
+  const CustomProgress({
+    this.color,
+    this.size = 20,
+  });
 
   @override
   Widget build(BuildContext context) {
-    Color _color = (color != null) ? color! : Theme.of(context).accentColor;
+    Color _color = (color != null) ? color! : kPrimaryColor;
 
     var brightness = Theme.of(context).brightness;
 
-    Widget widget;
-
-    if (ignorePlatform) {
-      widget = CircularProgressIndicator(
-        strokeWidth: 2.5,
-        valueColor: AlwaysStoppedAnimation<Color>(_color),
-      );
-    } else {
-      widget = (Platform.isIOS)
-          ? MediaQuery(
-          data: MediaQueryData(platformBrightness: brightness),
-          child: CupertinoActivityIndicator())
-          : CircularProgressIndicator(
-        strokeWidth: 2.5,
-        valueColor: AlwaysStoppedAnimation<Color>(_color),
-      );
-    }
-
     return Container(
-      child: widget,
+      child: (Platform.isIOS)
+          ? MediaQuery(
+              data: MediaQueryData(
+                platformBrightness: brightness,
+              ),
+              child: CupertinoActivityIndicator())
+          : CircularProgressIndicator(
+              strokeWidth: 2.5,
+              valueColor: AlwaysStoppedAnimation<Color>(_color),
+            ),
       width: size,
       height: size,
     );
