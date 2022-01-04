69056b8301c3983c998d41866ccf8055cd1f7c6f
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2022-01-04 08:18:55 +0100

    metric model

---
+++ lib/src/domain/packages/entities/metric.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_package/src/domain/packages/entities/score.dart';
+
+class Metric {
+  final List<String> tags;
+  final bool isNullSafe;
+  final Score score;
+
+  Metric({
+    required this.tags,
+    required this.isNullSafe,
+    required this.score,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'platformList': this.tags,
+      'isNullSafe': this.isNullSafe,
+      'score': this.score.toMap(),
+    };
+  }
+}
