3bfc054ee0d46cecac19d35b847c2b2a1a5e913f
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2022-07-18 19:59:21 +0100

    add data for mock

---
+++ test/src/infrastructure/core/data/package_fake_data.dart
@@ -0,0 +1,262 @@
+import 'package:flutter_package/src/infrastructure/core/mapper.dart';
+
+final defaultPackage = Mapper.packageFromMap({
+  "name": "avatar_letter",
+  "latest": {
+    "version": "1.0.1",
+    "pubspec": {
+      "version": "1.0.1",
+      "name": "avatar_letter",
+      "dependencies": {
+        "flutter": {
+          "sdk": "flutter"
+        }
+      },
+      "flutter": {
+        "uses-material-design": true
+      },
+      "author": "Edvaldo Martins <edvaldonuniomartins@gmail.com>",
+      "description": "It is a widget that uses the first few letters of a word to create an avatar with a set of basic functions..",
+      "homepage": "https://github.com/EdvaldoMartins/avatar_letter",
+      "environment": {
+        "sdk": ">=2.1.0 <3.0.0"
+      },
+      "dev_dependencies": {
+        "flutter_test": {
+          "sdk": "flutter"
+        }
+      }
+    },
+    "archive_url": "https://pub.dartlang.org/packages/avatar_letter/versions/1.0.1.tar.gz",
+    "published": "2019-09-17T14:00:31.765102Z"
+  },
+  "versions": [
+    {
+      "version": "1.0.0",
+      "pubspec": {
+        "version": "1.0.0",
+        "name": "avatar_letter",
+        "dependencies": {
+          "cupertino_icons": "^0.1.2",
+          "flutter": {
+            "sdk": "flutter"
+          }
+        },
+        "flutter": {
+          "uses-material-design": true
+        },
+        "author": "Edvaldo Martins <edvaldonuniomartins@gmail.com>",
+        "description": "It is a widget that uses the first few letters of a word to create an avatar with a set of basic functions..",
+        "homepage": "https://github.com/EdvaldoMartins/avatar_letter",
+        "environment": {
+          "sdk": ">=2.1.0 <3.0.0"
+        },
+        "dev_dependencies": {
+          "flutter_test": {
+            "sdk": "flutter"
+          }
+        }
+      },
+      "archive_url": "https://pub.dartlang.org/packages/avatar_letter/versions/1.0.0.tar.gz",
+      "published": "2019-09-17T13:18:07.978522Z"
+    },
+    {
+      "version": "1.0.1",
+      "pubspec": {
+        "version": "1.0.1",
+        "name": "avatar_letter",
+        "dependencies": {
+          "flutter": {
+            "sdk": "flutter"
+          }
+        },
+        "flutter": {
+          "uses-material-design": true
+        },
+        "author": "Edvaldo Martins <edvaldonuniomartins@gmail.com>",
+        "description": "It is a widget that uses the first few letters of a word to create an avatar with a set of basic functions..",
+        "homepage": "https://github.com/EdvaldoMartins/avatar_letter",
+        "environment": {
+          "sdk": ">=2.1.0 <3.0.0"
+        },
+        "dev_dependencies": {
+          "flutter_test": {
+            "sdk": "flutter"
+          }
+        }
+      },
+      "archive_url": "https://pub.dartlang.org/packages/avatar_letter/versions/1.0.1.tar.gz",
+      "published": "2019-09-17T14:00:31.765102Z"
+    }
+  ]
+});
+
+final defaultMetric = Mapper.metricFromMap({
+  "score": {
+    "grantedPoints": 75,
+    "maxPoints": 130,
+    "likeCount": 10,
+    "popularityScore": 0.8305305885763989,
+    "tags": [
+      "sdk:flutter",
+      "platform:android",
+      "platform:ios",
+      "platform:windows",
+      "platform:linux",
+      "platform:macos",
+      "platform:web",
+      "license:apache-2.0",
+      "license:fsf-libre",
+      "license:osi-approved"
+    ],
+    "lastUpdated": "2022-07-14T07:42:46.607237Z"
+  },
+  "scorecard": {
+    "packageName": "avatar_letter",
+    "packageVersion": "1.0.1",
+    "runtimeVersion": "2022.07.05",
+    "updated": "2022-07-14T07:42:46.607237Z",
+    "packageCreated": "2019-09-17T13:18:08.064386Z",
+    "packageVersionCreated": "2019-09-17T14:00:31.765102Z",
+    "flags": [
+      "latest-stable",
+      "uses-flutter"
+    ],
+    "dartdocReport": {
+      "timestamp": "2022-07-07T08:56:11.091564Z",
+      "reportStatus": "success",
+      "dartdocEntry": {
+        "uuid": "b1453201-3c8d-4041-90f5-7960fc21d137",
+        "packageName": "avatar_letter",
+        "packageVersion": "1.0.1",
+        "isLatest": true,
+        "isObsolete": false,
+        "usesFlutter": true,
+        "runtimeVersion": "2022.07.05",
+        "sdkVersion": "2.17.5",
+        "dartdocVersion": "5.1.2",
+        "flutterVersion": "3.0.4",
+        "timestamp": "2022-07-07T08:56:09.505117Z",
+        "runDuration": 114236394,
+        "depsResolved": true,
+        "hasContent": true,
+        "archiveSize": 151112,
+        "totalSize": 612158,
+        "blobSize": 184047,
+        "blobIndexSize": 1189
+      },
+      "documentationSection": {
+        "id": "documentation",
+        "title": "Provide documentation",
+        "grantedPoints": 0,
+        "maxPoints": 10,
+        "status": "failed",
+        "summary": "### [x] 0/10 points: 20% or more of the public API has dartdoc comments\n\n* 2 out of 29 API elements (6.9 %) have documentation comments.\n\nProviding good documentation for libraries, classes, functions, and other API elements improves code readability and helps developers find and use your API. Document at least 20% of the public API elements."
+      }
+    },
+    "panaReport": {
+      "timestamp": "2022-07-14T07:42:46.562860Z",
+      "panaRuntimeInfo": {
+        "panaVersion": "0.21.14",
+        "sdkVersion": "2.17.5",
+        "flutterVersions": {
+          "frameworkVersion": "3.0.4",
+          "channel": "unknown",
+          "repositoryUrl": "unknown source",
+          "frameworkRevision": "85684f9300908116a78138ea4c6036c35c9a1236",
+          "frameworkCommitDate": "2022-06-30 13:22:47 -0700",
+          "engineRevision": "6ba2af10bb05c88a2731482cedf2cfd11cf5af0b",
+          "dartSdkVersion": "2.17.5",
+          "devToolsVersion": "2.12.2",
+          "flutterRoot": "/tool/stable/flutter"
+        }
+      },
+      "reportStatus": "success",
+      "derivedTags": [
+        "sdk:flutter",
+        "platform:android",
+        "platform:ios",
+        "platform:windows",
+        "platform:linux",
+        "platform:macos",
+        "platform:web",
+        "license:apache-2.0",
+        "license:fsf-libre",
+        "license:osi-approved"
+      ],
+      "allDependencies": [
+        "characters",
+        "collection",
+        "flutter",
+        "flutter_test",
+        "material_color_utilities",
+        "meta",
+        "sky_engine",
+        "vector_math"
+      ],
+      "licenses": [
+        {
+          "path": "LICENSE",
+          "spdxIdentifier": "Apache-2.0"
+        }
+      ],
+      "report": {
+        "sections": [
+          {
+            "id": "convention",
+            "title": "Follow Dart file conventions",
+            "grantedPoints": 15,
+            "maxPoints": 20,
+            "status": "failed",
+            "summary": "### [*] 10/10 points: Provide a valid `pubspec.yaml`\n\n\n### [*] 5/5 points: Provide a valid `README.md`\n\n\n### [x] 0/5 points: Provide a valid `CHANGELOG.md`\n\n<details>\n<summary>\nCHANGELOG.md is empty.\n</summary>\n\nChangelog entries help developers follow the progress of your package. See the [example](https://raw.githubusercontent.com/dart-lang/stagehand/master/templates/package-simple/CHANGELOG.md) generated by `stagehand`.\n</details>"
+          },
+          {
+            "id": "documentation",
+            "title": "Provide documentation",
+            "grantedPoints": 0,
+            "maxPoints": 10,
+            "status": "failed",
+            "summary": "### [x] 0/10 points: Package has an example\n\n<details>\n<summary>\nNo example found.\n</summary>\n\nSee [package layout](https://dart.dev/tools/pub/package-layout#examples) guidelines on how to add an example.\n</details>"
+          },
+          {
+            "id": "platform",
+            "title": "Platform support",
+            "grantedPoints": 20,
+            "maxPoints": 20,
+            "status": "passed",
+            "summary": "### [*] 20/20 points: Supports 6 of 6 possible platforms (**iOS**, **Android**, **Web**, **Windows**, **MacOS**, **Linux**)\n\n* ✓ Android\n* ✓ iOS\n* ✓ Windows\n* ✓ Linux\n* ✓ MacOS\n* ✓ Web"
+          },
+          {
+            "id": "analysis",
+            "title": "Pass static analysis",
+            "grantedPoints": 20,
+            "maxPoints": 30,
+            "status": "partial",
+            "summary": "### [~] 20/30 points: code has no errors, warnings, lints, or formatting issues\n\n<details>\n<summary>\nINFO: This class (or a class that this class inherits from) is marked as '@immutable', but one or more of its instance fields aren't final: AvatarLetter.letterType, AvatarLetter.backgroundColor, AvatarLetter.backgroundColorHex, AvatarLetter.textColor, AvatarLetter.textColorHex, AvatarLetter.size\n</summary>\n\n`lib/avatar_letter.dart:18:7`\n\n```\n   ╷\n18 │ class AvatarLetter extends StatelessWidget {\n   │       ^^^^^^^^^^^^\n   ╵\n```\n\nTo reproduce make sure you are using the [lints_core](https://pub.dev/packages/lints) and run `flutter analyze lib/avatar_letter.dart`\n</details>\n<details>\n<summary>\nINFO: This function has a return type of 'String', but doesn't end with a return statement.\n</summary>\n\n`lib/avatar_letter.dart:79:10`\n\n```\n   ╷\n79 │   String _textConfig() {\n   │          ^^^^^^^^^^^\n   ╵\n```\n\nTo reproduce make sure you are using the [lints_core](https://pub.dev/packages/lints) and run `flutter analyze lib/avatar_letter.dart`\n</details>"
+          },
+          {
+            "id": "dependency",
+            "title": "Support up-to-date dependencies",
+            "grantedPoints": 20,
+            "maxPoints": 20,
+            "status": "passed",
+            "summary": "### [*] 10/10 points: All of the package dependencies are supported in the latest version\n\n|Package|Constraint|Compatible|Latest|\n|:-|:-|:-|:-|\n|[`flutter`]|`flutter`|0.0.0|0.0.0|\n\n<details><summary>Transitive dependencies</summary>\n\n|Package|Constraint|Compatible|Latest|\n|:-|:-|:-|:-|\n|[`characters`]|-|1.2.0|1.2.1|\n|[`collection`]|-|1.16.0|1.16.0|\n|[`material_color_utilities`]|-|0.1.4|0.1.5|\n|[`meta`]|-|1.7.0|1.8.0|\n|[`sky_engine`]|-|0.0.99|0.0.99|\n|[`vector_math`]|-|2.1.2|2.1.2|\n</details>\n\nTo reproduce run `dart pub outdated --no-dev-dependencies --up-to-date --no-dependency-overrides`.\n\n[`flutter`]: https://pub.dev/packages/flutter\n[`characters`]: https://pub.dev/packages/characters\n[`collection`]: https://pub.dev/packages/collection\n[`material_color_utilities`]: https://pub.dev/packages/material_color_utilities\n[`meta`]: https://pub.dev/packages/meta\n[`sky_engine`]: https://pub.dev/packages/sky_engine\n[`vector_math`]: https://pub.dev/packages/vector_math\n\n\n### [*] 10/10 points: Package supports latest stable Dart and Flutter SDKs\n"
+          },
+          {
+            "id": "null-safety",
+            "title": "Support sound null safety",
+            "grantedPoints": 0,
+            "maxPoints": 20,
+            "status": "partial",
+            "summary": "### [~] 0/20 points: Package does not opt in to null safety.\n\n<details>\n<summary>\nPackage language version (indicated by the sdk constraint `>=2.1.0 <3.0.0`) is less than 2.12.\n</summary>\n\nConsider [migrating](https://dart.dev/null-safety/migration-guide).\n</details>"
+          }
+        ]
+      },
+      "result": {
+        "homepageUrl": "https://github.com/EdvaldoMartins/avatar_letter"
+      },
+      "screenshots": [],
+      "urlProblems": []
+    }
+  }
+});
