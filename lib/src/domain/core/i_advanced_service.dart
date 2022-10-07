96cfd3b4a7d3de7efab28db5ffcb9c7948dab77c
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2022-10-07 13:48:23 +0100

    read generic doc .md

--- lib/src/domain/core/i_advanced_service.dart
+++ lib/src/domain/core/i_advanced_service.dart
@@ -2,8 +2,8 @@ import 'package:dartz/dartz.dart';
 import 'package:flutter_package/src/domain/core/request_failure.dart';
 
 abstract class IAdvancedService {
-  /// [getReadme]  Responsible for reading the project readme file
+  /// [readFile]  Responsible for reading the project readme file
   /// This return [String] when the request is successful
   /// and [RequestFailure] when the request is not successful
-  Future<Either<RequestFailure, String>> getReadme({required String gitPath});
+  Future<Either<RequestFailure, String>> readFile({required String gitPath,required String fileName});
 }
