c76bc6ebfb3ab9d1576d6e9cabadf461698bf606
Edvaldo Martins <edvaldonuniomartins@gmail.com>
2022-10-07 11:09:22 +0100

    add test for details package

---
+++ test/src/presentation/package/detail_package_page_test.dart
@@ -0,0 +1,55 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_package/src/application/packages/detail_package_view_model.dart';
+import 'package:flutter_package/src/domain/packages/i_package_service.dart';
+import 'package:flutter_package/src/infrastructure/packages/base_package_service.dart';
+import 'package:flutter_package/src/presentation/core/styles.dart';
+import 'package:flutter_package/src/presentation/packages/detail_package_screen.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../infrastructure/core/data/package_fake_data.dart';
+import '../../infrastructure/mocks.mocks.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  late MockIPackageRepository repository;
+  late MockIAdvancedService iAdvancedService;
+  late IPackageService service;
+  late String namePackage;
+  late DetailPackageViewModel viewModel;
+
+  setUp(() {
+    repository = MockIPackageRepository();
+    service = BasePackageService(repository);
+    iAdvancedService = MockIAdvancedService();
+    viewModel = DetailPackageViewModel(service, iAdvancedService);
+    namePackage = 'avatar_letter';
+  });
+
+  Widget createWidgetForTest() {
+    return MaterialApp(
+      theme: lightTheme(),
+      home: DetailPackageScreen(namePackage, viewModel),
+    );
+  }
+
+  testWidgets('test has data for detail', (tester) async {
+    when(repository.getPackageName(namePackage: namePackage))
+        .thenAnswer((_) async => Right(defaultPackage));
+
+    when(repository.getMetricPackage(package: namePackage))
+        .thenAnswer((_) async => Right(defaultMetric));
+
+    when(iAdvancedService.getReadme(
+            gitPath: defaultPackage.latest.pubspec.homepage))
+        .thenAnswer((_) async => Right(defaultReadme));
+
+    await tester.pumpWidget(createWidgetForTest());
+
+    await viewModel.load(namePackage);
+
+    expect(viewModel.hasData, true);
+  });
+}
